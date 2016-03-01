f792c2ee654eab733e2029dab88de8b122fd775e
joshcoales <joshua@coales.co.uk>
2016-03-01 19:22:33 +0000

    Reorganising and abandoning fxml. Maybe one day I'll figure out fxml, for now I'll skip it

--- src/main/java/uk/org/spangle/App.java
+++ src/main/java/uk/org/spangle/App.java
@@ -1,16 +1,36 @@
 package uk.org.spangle;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
+import javafx.collections.FXCollections;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 public class App extends Application {
 
+    HBox root;
+    VBox sideBar;
+    Pane infoPanel;
+
+
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        // Update side bar
+        updateSideBar();
+
+        // Update info panel
+        updateInfoPanel();
+
+        // Root HBox
+        root = new HBox();
+        root.getChildren().addAll(sideBar,infoPanel);
+
+        // Construct window and show.
         primaryStage.setTitle("Hello World");
         Scene primaryScene = new Scene(root, 300, 275);
         primaryScene.getStylesheets().add("sample.css");
@@ -21,5 +41,36 @@ public class App extends Application {
 
     public static void main(String[] args) {
         launch(args);
+    }
+
+    public void updateSideBar() {
+        Text titleText = new Text("Game");
+        ChoiceBox<String> gameDropdown = new ChoiceBox<>();
+        gameDropdown.setItems(FXCollections.observableArrayList(
+                "Pokebank", "Omega Ruby ", "Alpha Sapphire", "Save as")
+        );
+        Text boxText = new Text("Box");
+
+        Button boxLeft = new Button("<");
+        Button boxRight = new Button(">");
+        HBox boxButtons = new HBox();
+        boxButtons.getChildren().addAll(boxLeft,boxRight);
+
+        // Add everything to the sidebar
+        sideBar = new VBox();
+        sideBar.getChildren().addAll(titleText,gameDropdown,boxText,boxButtons);
+    }
+
+    public void updateInfoPanel() {
+        Text pokemonName = new Text("Pokemon");
+        Text pokemonDesc = new Text("Some descriptor");
+
+        VBox infoColumns = new VBox();
+        infoColumns.getChildren().addAll(pokemonName,pokemonDesc);
+        HBox infoBoxContents = new HBox();
+        infoBoxContents.getChildren().addAll(infoColumns);
+
+        infoPanel = new Pane();
+        infoPanel.getChildren().add(infoBoxContents);
     }
 }
