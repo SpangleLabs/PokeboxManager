df5a563605f47dafb25b7330e6ab77c103d9fe67
joshcoales <joshua@coales.co.uk>
2016-03-19 10:37:17 +0000

    Adding abilities to veekun data import

--- src/main/java/uk/org/spangle/tools/ImportVeekun.java
+++ src/main/java/uk/org/spangle/tools/ImportVeekun.java
@@ -5,13 +5,12 @@ import org.apache.commons.csv.CSVParser;
 import org.apache.commons.csv.CSVRecord;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
-import org.hibernate.criterion.Restrictions;
+import uk.org.spangle.data.Ability;
 import uk.org.spangle.data.Pokemon;
 import uk.org.spangle.data.PokemonForm;
 
 import java.io.File;
 import java.nio.charset.Charset;
-import java.nio.charset.IllegalCharsetNameException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -23,6 +22,9 @@ public class ImportVeekun {
     Session dbSession;
     public final static List<Integer> listGenderless = Arrays.asList(81,82,100,101,120,121,137,233,292,337,338,343,344,374,375,376,436,437,462,474,479,489,490,599,600,601,615,622,623,703);
     public final static List<Integer> listUnbreedable = Arrays.asList(132,144,145,146,150,151,201,243,244,245,249,250,251,377,378,379,382,383,384,385,386,480,481,482,483,484,486,487,491,492,493,494,638,639,640,643,644,646,647,648,649,716,717,718,719,720,721);
+    String languageId;
+    String versionGroupId;
+    String nationalDexId;
 
     public static void main(String[] args) {
         ImportVeekun imp = new ImportVeekun();
@@ -56,8 +58,18 @@ public class ImportVeekun {
             return;
         }
 
+        // Set up some handy variables
+        try {
+            languageId = getLanguageId("en");
+            versionGroupId = getVersionGroupId("omega-ruby-alpha-sapphire");
+            nationalDexId = getPokedexId("national");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
         // Try and create stuff
         try {
+            createAbilities();
             createPokemon();
         } catch (Exception e) {
             e.printStackTrace();
@@ -104,22 +116,33 @@ public class ImportVeekun {
         throw new IllegalArgumentException();
     }
 
-    private CSVRecord getPokemonRecordById(String pokemonId) throws Exception {
-        CSVParser parser = loadCSV("pokemon");
+    private String getAbilityNameById(String abilityId, String languageId) throws Exception {
+        CSVParser parser = loadCSV("ability_names");
         for (CSVRecord csvRecord : parser) {
-            if(csvRecord.get("id").equals(pokemonId)) {
-                return csvRecord;
+            if(!csvRecord.get("ability_id").equals(abilityId)) {
+                continue;
             }
+            if(!csvRecord.get("local_language_id").equals(languageId)) {
+                continue;
+            }
+            return csvRecord.get("name");
         }
         throw new IllegalArgumentException();
     }
 
-    private CSVRecord getPokemonSpeciesRecordById(String speciesId) throws Exception {
-        CSVParser parser = loadCSV("pokemon_species");
+    private String getAbilityDescById(String abilityId, String languageId, String versionGroupId) throws Exception {
+        CSVParser parser = loadCSV("ability_flavor_text");
         for (CSVRecord csvRecord : parser) {
-            if(csvRecord.get("id").equals(speciesId)) {
-                return csvRecord;
+            if(!csvRecord.get("ability_id").equals(abilityId)) {
+                continue;
             }
+            if(!csvRecord.get("version_group_id").equals(versionGroupId)) {
+                continue;
+            }
+            if(!csvRecord.get("language_id").equals(languageId)) {
+                continue;
+            }
+            return csvRecord.get("flavor_text");
         }
         throw new IllegalArgumentException();
     }
@@ -172,25 +195,21 @@ public class ImportVeekun {
         throw new IllegalArgumentException();
     }
 
-    public Pokemon getOrMakePokemon(int nationalDex, String name) {
-        // Search for pokemon by national dex
-        List listPokemon = dbSession.createCriteria(Pokemon.class).add(Restrictions.eq("nationalDex",nationalDex)).list();
-        if(listPokemon.size() == 0) {
-            // Not found it, make it.
-            Pokemon mon = new Pokemon(nationalDex,name);
-            mon.setIsGenderless(listGenderless.contains(nationalDex) || listUnbreedable.contains(nationalDex));
-            dbSession.save(mon);
-            return mon;
+    public void createAbilities() throws Exception {
+        // Load abilities list
+        CSVParser parser = loadCSV("abilities");
+        for (CSVRecord record : parser) {
+            String abilityId = record.get("id");
+            String isMain = record.get("is_main_series");
+            if(isMain.equals("0")) continue;
+            String abilityName = getAbilityNameById(abilityId,languageId);
+            String abilityDesc = getAbilityDescById(abilityId,languageId,versionGroupId);
+            Ability ability = new Ability(abilityName,abilityDesc);
+            dbSession.save(ability);
         }
-        return (Pokemon) listPokemon.get(0);
     }
 
     public void createPokemon() throws Exception {
-        // Load languages table
-        String languageId = getLanguageId("en");
-        /*String versionGroupId = getVersionGroupId("omega-ruby-alpha-sapphire");
-        String nationalDexId = getPokedexId("national");/**/
-
         // Load pokemon list
         CSVParser parser = loadCSV("pokemon_species");
         //Map<String,Ability> abilityMap = new HashMap<>();
