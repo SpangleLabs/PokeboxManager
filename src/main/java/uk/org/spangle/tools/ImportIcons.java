c03a4f874ed9dcf85a278e5edc76337ee9e017fa
joshcoales <joshua@coales.co.uk>
2016-03-19 00:16:35 +0000

    Neatening up ImportIcons tool

--- src/main/java/uk/org/spangle/tools/ImportIcons.java
+++ src/main/java/uk/org/spangle/tools/ImportIcons.java
@@ -5,10 +5,6 @@ import org.apache.commons.codec.digest.DigestUtils;
 import org.apache.commons.csv.CSVFormat;
 import org.apache.commons.csv.CSVParser;
 import org.apache.commons.csv.CSVRecord;
-import org.hibernate.Session;
-import org.hibernate.SessionFactory;
-import org.hibernate.criterion.Restrictions;
-import uk.org.spangle.data.Pokemon;
 
 import javax.imageio.ImageIO;
 import java.awt.*;
@@ -18,17 +14,12 @@ import java.io.FileInputStream;
 import java.io.PrintWriter;
 import java.nio.charset.Charset;
 import java.util.*;
-import java.util.List;
 import java.util.concurrent.ConcurrentHashMap;
 
 /**
- * Quick and easy tool to import the list of pokeballs.
- * Hardcoded in from information from serebii list
+ * Tool to load all pokemon forms, find the icons and merge them together into a big image and csv file
  */
 public class ImportIcons {
-    Session dbSession;
-    public final static List<Integer> listGenderless = Arrays.asList(81,82,100,101,120,121,137,233,292,337,338,343,344,374,375,376,436,437,462,474,479,489,490,599,600,601,615,622,623,703);
-    public final static List<Integer> listUnbreedable = Arrays.asList(132,144,145,146,150,151,201,243,244,245,249,250,251,377,378,379,382,383,384,385,386,480,481,482,483,484,486,487,491,492,493,494,638,639,640,643,644,646,647,648,649,716,717,718,719,720,721);
     public final static Map<String,String> filenameReplace = new HashMap<>();
     static {
         filenameReplace.put("furfrou-natural.png","furfrou.png");
@@ -48,28 +39,12 @@ public class ImportIcons {
     }
 
     public void run() {
-        // Create connection
-        SessionFactory sessionFactory = new org.hibernate.cfg.Configuration().configure(getClass().getResource("/hibernate.cfg.xml")).buildSessionFactory();
-        dbSession = sessionFactory.openSession();
-
-        // Check if abilities table is empty
-        List listGens = dbSession.createCriteria(Pokemon.class).list();
-        if(listGens.size() > 0) {
-            System.out.println("Pokemon have already been imported.");
-            dbSession.close();
-            sessionFactory.close();
-            return;
-        }
 
         // Try and create stuff
         try {
             createAbilities();
         } catch (Exception e) {
             e.printStackTrace();
-        } finally {
-            // Shut down cleanly
-            dbSession.close();
-            sessionFactory.close();
         }
     }
 
@@ -77,36 +52,6 @@ public class ImportIcons {
         String filename = "/pokedex/pokedex/data/csv/"+csvName+".csv";
         File csvData = new File(getClass().getResource(filename).getFile());
         return CSVParser.parse(csvData, Charset.defaultCharset(), CSVFormat.RFC4180.withHeader());
-    }
-
-    private String getLanguageId(String iso639Code) throws Exception {
-        CSVParser parser = loadCSV("languages");
-        for (CSVRecord csvRecord : parser) {
-            if(csvRecord.get("iso639").equals(iso639Code)) {
-                return csvRecord.get("id");
-            }
-        }
-        throw new IllegalArgumentException();
-    }
-
-    private String getVersionGroupId(String versionGroup) throws Exception {
-        CSVParser parser = loadCSV("version_groups");
-        for (CSVRecord csvRecord : parser) {
-            if(csvRecord.get("identifier").equals(versionGroup)) {
-                return csvRecord.get("id");
-            }
-        }
-        throw new IllegalArgumentException();
-    }
-
-    private String getPokedexId(String pokedex) throws Exception {
-        CSVParser parser = loadCSV("pokedexes");
-        for (CSVRecord csvRecord : parser) {
-            if(csvRecord.get("identifier").equals(pokedex)) {
-                return csvRecord.get("id");
-            }
-        }
-        throw new IllegalArgumentException();
     }
 
     private CSVRecord getPokemonRecordById(String pokemonId) throws Exception {
@@ -129,25 +74,7 @@ public class ImportIcons {
         throw new IllegalArgumentException();
     }
 
-    public Pokemon getOrMakePokemon(int nationalDex, String name) {
-        // Search for pokemon by national dex
-        List listPokemon = dbSession.createCriteria(Pokemon.class).add(Restrictions.eq("nationalDex",nationalDex)).list();
-        if(listPokemon.size() == 0) {
-            // Not found it, make it.
-            Pokemon mon = new Pokemon(nationalDex,name);
-            mon.setIsGenderless(listGenderless.contains(nationalDex) || listUnbreedable.contains(nationalDex));
-            dbSession.save(mon);
-            return mon;
-        }
-        return (Pokemon) listPokemon.get(0);
-    }
-
     public void createAbilities() throws Exception {
-        // Load languages table
-        /*String languageId = getLanguageId("en");
-        String versionGroupId = getVersionGroupId("omega-ruby-alpha-sapphire");
-        String nationalDexId = getPokedexId("national");/**/
-
         // Load abilities id list
         CSVParser parser = loadCSV("pokemon_forms");
         ConcurrentHashMap<String,Pair<Integer,Integer>> fileHashes = new ConcurrentHashMap<>();
@@ -288,16 +215,13 @@ public class ImportIcons {
                 bigCSV += tempX+","+tempY+"\n";
             }
 
-
-            /*System.out.println(csvRecord.get("form_identifier"));
-            System.out.println(csvRecord.get("pokemon_id"));
-            System.out.println(pokemonRecord.get("species_id"));
-            System.out.println(speciesIdentifier);*/
         }
         System.out.println(fileHashes.size());
         ImageIO.write(bigImage,"png",new File("result.png"));
         PrintWriter out = new PrintWriter("result.csv");
         out.print(bigCSV);
         out.close();
+
+        System.out.println("Now please move result.png and result.csv into resources directory before running ImportVeekun.");
     }
 }
