33535d5962e3252a7f4429d67de20bcb7261c360
joshcoales <joshua@coales.co.uk>
2016-03-22 11:09:42 +0000

    Adding Move table

---
+++ src/main/java/uk/org/spangle/data/Move.java
@@ -0,0 +1,49 @@
+package uk.org.spangle.data;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "move")
+public class Move {
+    private int id;
+    private String name;
+    private String description;
+
+    public Move() {
+        // this form used by Hibernate
+    }
+
+    public Move(String name, String description) {
+        this.name = name;
+        this.description = description;
+    }
+
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    @Column(name="ability_id")
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Column(name="name")
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Column(name="description")
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
