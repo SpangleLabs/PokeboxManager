2bbe51f36781e5ddcae7e4edb6de66e500aa3383
joshcoales <joshua@coales.co.uk>
2016-03-24 14:39:54 +0000

    Adding UserPokemonMove class

---
+++ src/main/java/uk/org/spangle/data/UserPokemonMove.java
@@ -0,0 +1,80 @@
+package uk.org.spangle.data;
+
+import javax.persistence.*;
+import java.sql.Timestamp;
+import java.util.Date;
+
+@Entity
+@Table(
+        name = "user_pokemon_move",
+        uniqueConstraints = @UniqueConstraint(columnNames={"user_pokemon_id", "move_slot_id"})
+)
+public class UserPokemonMove {
+
+    private int id;
+    private UserPokemon userPokemon;
+    private MoveSlot moveSlot;
+    private Move move;
+    private Timestamp timestamp;
+
+    public UserPokemonMove() {
+        // this form used by Hibernate
+    }
+
+    public UserPokemonMove(UserPokemon userPokemon, MoveSlot moveSlot, Move move) {
+        this.userPokemon = userPokemon;
+        this.moveSlot = moveSlot;
+        this.move = move;
+        Date date = new Date();
+        this.timestamp = new Timestamp(date.getTime());
+    }
+
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    @Column(name="user_pokemon_move_id")
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @OneToOne
+    @JoinColumn(name="user_pokemon_id")
+    public UserPokemon getUserPokemon() {
+        return userPokemon;
+    }
+
+    public void setUserPokemon(UserPokemon userPokemon) {
+        this.userPokemon = userPokemon;
+    }
+
+    @ManyToOne
+    @JoinColumn(name="move_slot_id")
+    public MoveSlot getMoveSlot() {
+        return moveSlot;
+    }
+
+    public void setMoveSlot(MoveSlot moveSlot) {
+        this.moveSlot = moveSlot;
+    }
+
+    @Column(name="move_id")
+    public Move getMove() {
+        return move;
+    }
+
+    public void setMove(Move move) {
+        this.move = move;
+    }
+
+    @Column(name = "timestamp")
+    public Timestamp getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(Timestamp timestamp) {
+        this.timestamp = timestamp;
+    }
+}
