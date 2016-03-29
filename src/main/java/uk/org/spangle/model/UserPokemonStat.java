233d83c83b5b6934036aed9fd293f627b6752cce
joshcoales <joshua@coales.co.uk>
2016-03-29 13:52:34 +0100

    Adding UserPokemonStat model class

---
+++ src/main/java/uk/org/spangle/model/UserPokemonStat.java
@@ -0,0 +1,85 @@
+package uk.org.spangle.model;
+
+import uk.org.spangle.data.*;
+
+public class UserPokemonStat {
+    UserPokemon userPokemon;
+    Stat stat;
+
+    public UserPokemonStat(UserPokemon userPokemon, Stat stat) {
+        this.userPokemon = userPokemon;
+        this.stat = stat;
+    }
+
+    public String getName() {
+        return stat.getName();
+    }
+
+    public String getIV() {
+        UserPokemonIV upi = userPokemon.getUserPokemonIV(stat);
+        if(upi == null) return "Unknown";
+        return Integer.toString(upi.getValue());
+    }
+
+    public String getEV() {
+        UserPokemonEV upe = userPokemon.getUserPokemonEV(stat);
+        if(upe == null) return "Unknown";
+        return Integer.toString(upe.getValue());
+    }
+
+    public String getBaseStat() {
+        // TODO
+        return null;
+    }
+
+    public String getValue() {
+        int value = -1;
+        if(stat.getAbbr().equals(Configuration.STAT_HP)) {
+            value = getHPValue();
+        } else {
+            value = getStatValue();
+        }
+        if(value == -1) return "Unknown";
+        return Integer.toString(value);
+    }
+
+    private int getHPValue() {
+        UserPokemonIV upi = userPokemon.getUserPokemonIV(stat);
+        UserPokemonEV upe = userPokemon.getUserPokemonEV(stat);
+        UserPokemonLevel upl = userPokemon.getUserPokemonLevel();
+        UserPokemonForm upf = userPokemon.getUserPokemonForm();
+        if(upi == null || upe == null || upl == null || upf == null) {
+            return -1;
+        }
+        PokemonFormBaseStat pfbs = upf.getPokemonForm().getPokemonFormBaseStat(stat);
+        int base = pfbs.getValue();
+        int iv = upi.getValue();
+        int ev = upe.getValue();
+        int level = upl.getLevel();
+        return ((2*base+iv+(ev/4))*level)/100+level+10;
+    }
+
+    private int getStatValue() {
+        UserPokemonIV upi = userPokemon.getUserPokemonIV(stat);
+        UserPokemonEV upe = userPokemon.getUserPokemonEV(stat);
+        UserPokemonLevel upl = userPokemon.getUserPokemonLevel();
+        UserPokemonForm upf = userPokemon.getUserPokemonForm();
+        UserPokemonNature upn = userPokemon.getUserPokemonNature();
+        if(upi == null || upe == null || upl == null || upf == null || upn == null) {
+            return -1;
+        }
+        PokemonFormBaseStat pfbs = upf.getPokemonForm().getPokemonFormBaseStat(stat);
+        int base = pfbs.getValue();
+        int iv = upi.getValue();
+        int ev = upe.getValue();
+        int level = upl.getLevel();
+        double natureModifier = 1.0;
+        Stat upStat = upn.getNature().getStatUp();
+        Stat downStat = upn.getNature().getStatDown();
+        if(stat == upStat) natureModifier = 1.1;
+        if(stat == downStat) natureModifier = 0.9;
+        if(upStat == downStat) natureModifier = 1.0;
+        //TODO
+        return (int) ((((2*base+iv+(ev/4))*level)/100+5)*natureModifier);
+    }
+}
